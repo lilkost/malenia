3ed89fb186eb1ee6a64e52c2f7720d8959ed5366
lilkost <111429255+lilkost@users.noreply.github.com>
2024-07-14 19:05:24 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const img = document.getElementById('person');
+
+window.onmousemove = (e) => {
+    let x = (e.clientX - window.innerWidth) * -.01;
+    let y = (e.clientY - window.innerHeight) * -.01;
+    img.style = `--x: ${x}deg; --y: ${y}deg;`
+}
+
+const audio = document.querySelector('audio');
+window.addEventListener('click', ()=> {
+    const volume = 0.05;
+    audio.volume = volume;
+    audio.play();
+})
